0faa1785cc3ad8a2f0c9981a869ec8e312782f23
sayan-insta <g24ai1062@iitj.in>
2026-07-26 12:01:21 +0530

    Initial project structure

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("=" * 60)
+    print("        Welcome to PathGen-Nexus")
+    print("Cross-Modal Transcriptomic Generation System")
+    print("=" * 60)
+
+if __name__ == "__main__":
+    main()
